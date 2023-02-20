493d7664c052c62888f2be3721b326955ada892a
ROBERTPROLINE <36228404+ROBERTPROLINE@users.noreply.github.com>
2023-02-20 11:21:13 +0000

    Update dbconnect.js

--- database/dbconnect.js
+++ database/dbconnect.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const url = "mongo://" //use you mongodb server connection
+const url = "mongodb://localhost:27017" //use you mongodb server connection // i use mogodb-compass comunity
 
 mongoose.connect(url, ()=>{
     console.log('database connected ')
