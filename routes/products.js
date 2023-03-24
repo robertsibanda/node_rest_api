dba36794c5b9188ac491d0adf9dda90d2d286478
ROBERTPROLINE <36228404+ROBERTPROLINE@users.noreply.github.com>
2023-03-24 11:00:50 +0200

    Delete products.js

--- routes/products.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-
-const { updateProduct, getProduct, getAllProducts, deleteProdut, createProduct } = require("../controllers/products");
-
-router.route("/").get(getAllProducts).post(createProduct);
-router.route("/:id").get(getProduct).patch(updateProduct).delete(deleteProdut);
-
-
-module.exports = router 
