785e72c54caef3afe4a1369f7423077cb63059ff
ROBERTPROLINE <36228404+ROBERTPROLINE@users.noreply.github.com>
2023-03-24 10:58:23 +0200

    Delete database directory

--- database/dbconnect.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const url = "mongodb://localhost:27017" //use you mongodb server connection // i use mogodb-compass comunity
-
-mongoose.connect(url, ()=>{
-    console.log('database connected ')
-});
-
